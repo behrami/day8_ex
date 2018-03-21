74c6a349eded6ff0b2ddcd45f62fd5117d662256
behrami <behram1993@gmail.com>
2018-03-21 14:30:55 -0400

    part1

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+fav_color= ["black", "red", "blue"]
+age = [23, 24 ,25, 20]
+heads = [true, true, false, true, false]
+artist= ["lil pump", "skrillex", "drake"]
+fav_color_sym = [:black, :red, :blue]
+
+word_def= {cringe: "when someone acts awkward and it makes you feel ashamed", edgy: "something or someone trying too hard to be cool", triggered: "getting angry"}
+fav_movies={black_panther: 2018, thor_ragnarok: 2017, the_shape_of_water: 2018}
+city_pop={toronto: "2.8 Million", mumbai: "21 Million", kyoto: "1.5 Million"}
+names= {alex: 24, alexa: 25, bob: 30, kevin:28}
+
+p heads
+puts fav_color[0]
+p age.sort
+p age.sort.push(0)
